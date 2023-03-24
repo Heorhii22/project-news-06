ac5aa212ede3d8e6989983e5440dbbf325cc3a3f
Heorhii22 <113346839+Heorhii22@users.noreply.github.com>
2023-03-24 21:28:43 +0000

    Deploying to gh-pages from @ Heorhii22/project-news-06@4b90bd7db773a8eb1944d655149068eda71f9172 🚀

---
+++ read.d0887bd5.js
@@ -0,0 +1,2 @@
+for(var accordion=document.querySelector(".accordion"),i=0;i<accordion.length;i++)accordion[i].addEventListener("click",(function(){this.classList.toggle("active")}));
+//# sourceMappingURL=read.d0887bd5.js.map
