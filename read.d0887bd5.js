f0b88e39cc2d8edc901a97524fe6e5628d36a1b1
Heorhii22 <113346839+Heorhii22@users.noreply.github.com>
2023-03-27 05:27:33 +0000

    Deploying to gh-pages from @ Heorhii22/project-news-06@a05ba80fac7cfdeeea93f2ad230ef11c9596ba09 🚀

--- read.d0887bd5.js
+++
@@ -1,2 +0,0 @@
-for(var accordion=document.querySelector(".accordion"),i=0;i<accordion.length;i++)accordion[i].addEventListener("click",(function(){this.classList.toggle("active")}));
-//# sourceMappingURL=read.d0887bd5.js.map
